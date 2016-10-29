eef8d115457e355e599085f5acdee7cdd9c4b83f
zhangwei725 <zw520725>
2016-10-29 17:39:22 +0800

    框架分包 添加统一管理的包版本管理

---
+++ retrofitlib/src/main/java/com/zw/retrofitlib/RetrofitManager.java
@@ -0,0 +1,8 @@
+package com.zw.retrofitlib;
+
+/**
+ * Created by zhangwei on 16/10/29.
+ */
+
+public class RetrofitManager {
+}
--- rxlibs/src/androidTest/java/com/zw/rxlibs/ExampleInstrumentedTest.java
+++ rxlibs/src/androidTest/java/com/zw/rxlibs/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.zw.retrofitlib;
+package com.zw.rxlibs;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.zw.retrofitlib.test", appContext.getPackageName());
+        assertEquals("com.zw.rxlibs.test", appContext.getPackageName());
     }
 }
